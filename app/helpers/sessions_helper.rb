2f88edcd3bc74fdf43d8276b62a5b6ba846e83cb
NickGY <kolyagogol@email.com>
2022-10-07 10:46:03 +0300

    Add account activation

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,7 +15,7 @@ module SessionsHelper
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.encrypted[:user_id])
         user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
+      if user && user.authenticated?(:remember,cookies[:remember_token])
         log_in user
         @current_user = user
       end
@@ -33,7 +33,7 @@ module SessionsHelper
   def forget(user)
     user.forget
     cookies.delete(:user_id)
-    cookies.delete(:remeber_token)
+    cookies.delete(:remember_token)
   end
  
   def log_out
